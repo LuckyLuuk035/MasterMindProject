343c88c52a79695852a038dd234d0f4d5eb9f639
LuckyLuuk035 <60790903+LuckyLuuk035@users.noreply.github.com>
2020-02-13 10:45:07 +0100

    Add files via upload

---
+++ MasterMind.py
@@ -0,0 +1,9 @@
+allOptions = []
+for a in range(1,7):
+    for b in range(1,7):
+        for c in range(1,7):
+            for d in range(1,7):
+                allOptions.append(str(a)+str(b)+str(c)+str(d))
+
+trueKey = '1122'
+
